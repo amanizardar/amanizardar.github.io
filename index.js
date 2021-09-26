3ea6fc4f06427fe1e6869af0e76bca89dcc64d2a
Aman Izardar <amanizardar@gmail.com>
2021-09-26 21:01:31 +0530

    Added Contact Page

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function myfun(s)
     if(s=='home')
     {
         // console.log("home");
-        i=0;
+        // i=0;
         document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("home_content")[0].innerHTML;
-        typeWriter();
+        // typeWriter();
+        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
+
         
         
     }
@@ -14,26 +16,40 @@ function myfun(s)
     {
         // console.log("Education");
         document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Education_content")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
+        
     }
     else if(s=="Project")
     {
         document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Project_content")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
+
     }
     else if(s=="Contact")
     {
         document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Contact_content")[0].innerHTML;
+        
+        // trying new things:
+        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("Contact_content_lw")[0].innerHTML;
+
+        // document.getElementsByClassName("left_window")[0].innerHTML="Contact Me";
+        // document.getElementById("rw").style.marginLeft="0px";
+        // document.getElementById("rw").style.marginRight="600px";
+
+
+
     }
 }
 
-var i=0;
-var txt = 'Hi There, My Name is Aman Izardar.';
-var speed = 50;
-function typeWriter() {
+// var i=0;
+// var txt = 'Hi There, My Name is Aman Izardar.';
+// var speed = 50;
+// function typeWriter() {
 
-  if (i < txt.length) {
-    document.getElementById("typewrite").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+//   if (i < txt.length) {
+//     document.getElementById("typewrite").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
 
