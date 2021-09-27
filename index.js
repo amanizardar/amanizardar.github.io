5d3589e5c4b8a8e9c82f5b9ea43d8efc80d67985
Aman Izardar <amanizardar@gmail.com>
2021-09-28 00:58:40 +0530

    Almost All Done

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-function myfun(s)
-{
-    
-    if(s=='home')
-    {
+function myfun(s) {
+
+    if (s == 'home') {
         // console.log("home");
         // i=0;
-        document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("home_content")[0].innerHTML;
+        document.getElementsByClassName("right_window")[0].innerHTML = document.getElementsByClassName("home_content")[0].innerHTML;
         // typeWriter();
-        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML = document.getElementsByClassName("home_content_lw")[0].innerHTML;
 
-        
-        
+
+
     }
-    else if(s=='Education')
-    {
+    else if (s == 'Education') {
         // console.log("Education");
-        document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Education_content")[0].innerHTML;
-        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
-        
-    }
-    else if(s=="Project")
-    {
-        document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Project_content")[0].innerHTML;
-        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("home_content_lw")[0].innerHTML;
+        document.getElementsByClassName("right_window")[0].innerHTML = document.getElementsByClassName("Education_content")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML = document.getElementsByClassName("home_content_lw")[0].innerHTML;
 
     }
-    else if(s=="Contact")
-    {
-        document.getElementsByClassName("right_window")[0].innerHTML=document.getElementsByClassName("Contact_content")[0].innerHTML;
-        
+    else if (s == "Project") {
+        document.getElementsByClassName("right_window")[0].innerHTML = document.getElementsByClassName("Project_content")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML = document.getElementsByClassName("home_content_lw")[0].innerHTML;
+
+    }
+    else if (s == "Contact") {
+        document.getElementsByClassName("right_window")[0].innerHTML = document.getElementsByClassName("Contact_content")[0].innerHTML;
+
         // trying new things:
-        document.getElementsByClassName("left_window")[0].innerHTML=document.getElementsByClassName("Contact_content_lw")[0].innerHTML;
+        document.getElementsByClassName("left_window")[0].innerHTML = document.getElementsByClassName("Contact_content_lw")[0].innerHTML;
 
         // document.getElementsByClassName("left_window")[0].innerHTML="Contact Me";
         // document.getElementById("rw").style.marginLeft="0px";
@@ -40,15 +35,13 @@ function myfun(s)
 
     }
 }
-function start()
-{
+function start() {
     startdt();
     startclk();
-    
+
 }
 
-function startclk()
-{
+function startclk() {
     let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
@@ -56,26 +49,25 @@ function startclk()
     h = refine(h);
     m = refine(m);
     s = refine(s);
-    document.getElementById("clk").innerHTML=h+":"+m+":"+s;
-    setInterval(startclk,1000);
-    
+    document.getElementById("clk").innerHTML = h + ":" + m + ":" + s;
+    setInterval(startclk, 1000);
+
 }
 
-function startdt()
-{
+function startdt() {
     let date = new Date();
-    let d=refine(date.getDate());
-    let mo=refine(date.getMonth());
-    let y=date.getFullYear();
-    document.getElementById("dt").innerHTML=d+"/"+mo+"/"+y;
+    let d = refine(date.getDate());
+    let mo = refine(date.getMonth());
+    let y = date.getFullYear();
+    document.getElementById("dt").innerHTML = d + "/" + mo + "/" + y;
     // setInterval(startdt,1000);
-    
+
 }
 
-function refine(s)
-{
-    if(s<10){
-    s="0"+s;}
+function refine(s) {
+    if (s < 10) {
+        s = "0" + s;
+    }
     return s;
 }
 // var i=0;
